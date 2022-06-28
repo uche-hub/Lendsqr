b17f58f335bf4e65f48fd41f30909da7869fc6b0
MAC <mac@MACs-MacBook-Pro.local>
2022-06-28 10:32:45 +0100

    Adding form button

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { LogoIcon, LogoText } from '../../component/icons/index.js'
+import './login.scss';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="left-side">
+        <div className="login-logos">
+          <LogoIcon />
+          <div  className="logoText">
+            <LogoText />
+          </div>
+        </div>
+        <img className='loginImg' src={require ('../../images/login.jpg')} alt="login image" srcset="" />
+      </div>
+      <div className="right-side">
+        <div className="loginForm">
+          <div className="heading">
+            <h1 className='headingText'>Welcome!</h1>
+            <p className='headingSub'>Enter details to login.</p>
+          </div>
+
+          <div className="form">
+            <form>
+              <input type="email" placeholder='Email' />
+              <input type="password" placeholder='Password' />
+
+              <p className='forgotPassword'>Forgot Password?</p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
