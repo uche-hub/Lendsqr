a15e532e4d283c56aacddc36b5baca4a9aabe548
MAC <mac@MACs-MacBook-Pro.local>
2022-06-29 15:18:59 +0100

    incomplete dashboard

---
+++ src/component/userCard/UserCard.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { UserActive, Users, UsersWithLoans, UsersWithSavings } from '../icons';
+import './userCard.scss'
+
+const UserCard = () => {
+  return (
+    <div className='userCardHeader'>
+      <h1 className='userCardText'>Users</h1>
+
+      {/* ==================== CARDS ==================*/}
+      <div className="userCardContainer">
+        <div className="userCard">
+            <div className="userCardDetails">
+                <Users />
+                <p className='userCardDetailText userCardDetail1'>USERS</p>
+                <p className='userCardDetailText userCardDetail2'>2,453</p>
+            </div>
+        </div>
+        <div className="userCard">
+            <div className="userCardDetails">
+                <UserActive />
+                <p className='userCardDetailText userCardDetail1'>ACTIVE USERS</p>
+                <p className='userCardDetailText userCardDetail2'>2,453</p>
+            </div>
+        </div>
+        <div className="userCard">
+            <div className="userCardDetails">
+                <UsersWithLoans />
+                <p className='userCardDetailText userCardDetail1'>USERS WITH LOANS</p>
+                <p className='userCardDetailText userCardDetail2'>2,453</p>
+            </div>
+        </div>
+        <div className="userCard">
+            <div className="userCardDetails">
+                <UsersWithSavings />
+                <p className='userCardDetailText userCardDetail1'>USERS WITH SAVINGS</p>
+                <p className='userCardDetailText userCardDetail2'>2,453</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserCard;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../component/nav/Navbar';
 import Sidebar from '../../component/sidebar/Sidebar';
+import UserCard from '../../component/userCard/UserCard';
 
 const Dashboard = () => {
   return (
     <div className='background'>
       <Navbar />
       <Sidebar />
+      <UserCard />
     </div>
   )
 }
